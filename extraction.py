e1a36e69b879a3d72c4973a7610da58f0265b6ad
jckoontz <jckoontz@indiana.edu>
2017-05-26 10:46:32 -0400

    Update extraction.py

--- extraction.py
+++ extraction.py
@@ -9,8 +9,8 @@ import operator
 import sys
 import codecs
 
-os.environ['STANFORD_PARSER'] = '//Users//Charles//Desktop//stanford-parser-full-2016-10-31'
-os.environ['CLASSPATH'] = '//Users//Charles//Desktop//stanford-parser-full-2016-10-31'
+os.environ['STANFORD_PARSER'] = 'Add Path'
+os.environ['CLASSPATH'] = 'Add Path'
 
 parser = StanfordParser()
 
@@ -31,43 +31,3 @@ for sent in sent_list:
     sent = ParentedTree.convert(sent)
     print(sent)
 
-#tree = list(parser.raw_parse("Yo he pagado al chico."))[0]
-#tree = ParentedTree.convert(tree)
-#print(tree)
-#tree.pretty_print()
-
-
-
-#text = sent_tokenize(text)
-#tree = et.parse('input.txt.xml')
-#root = tree.getroot()
-#print(root.tag, root.attrib)
-#rootd = root.find('dependent')
-#print(rootd.tag, rootd.attrib)
-
-#with open('inputtxt.xml', 'rt') as f:
-    #tree = ElementTree.parse(f)
-
-
-
-#for node in tree.iter():
- #   print (node.tag, node.attrib) 
-
-#for child in root:
- #   print(child.tag, child.attrib)
-
-#for elem in page(root): 
- #   dep = page.find('dep').text
-  #  print(dep)
-
-#for dep in root.iter('dep'):
- #   dep = dep.find('dep').text
-  #  print(dep)
-   # print(dependent.attrib)
-
-#for elem in root.findall('governor'):
- #   print (elem.tag, elem.attrib)
-#for dependencies in root.iter('dependencies'):
- #   print(dependencies.attrib)
-  #  name = dependencies.get('dependencies')
-   # print(name)
